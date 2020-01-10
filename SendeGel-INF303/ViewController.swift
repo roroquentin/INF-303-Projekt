b67e7f769a28cda7595cbcc5447891d39c0ec810
Hasan <hasan.guzelmansur@gmail.com>
2020-01-10 19:32:06 +0300

    Initial Commit

---
+++ SendeGel-INF303/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendeGel-INF303
+//
+//  Created by Hasan Güzelmansur on 10.01.2020.
+//  Copyright © 2020 Hasan Güzelmansur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
